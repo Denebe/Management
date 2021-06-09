f10c9560a434ab1a30ef983025a6601b0ce9d53a
denebe <whtjdfo01@gmail.com>
2021-06-09 13:48:09 +0900

    initalize node.js Mysql Connection

---
+++ management/client/src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import Customer from './components/Customer';
+
+const GloblaStyle = createGlobalStyle`
+  body{
+    width: 100%;
+    margin: 0 auto;
+  }
+`;
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+`;
+
+const Thead = styled.thead`
+  .tr{
+    background-color: #009879;
+    color: #ffffff;
+    text-align: left;
+  }
+
+  .td{
+    padding: 12px 15px;
+  }
+`;
+
+class App extends Component {
+
+  state = {
+    customers: ''
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+  render() {
+    return (
+      <>
+        <GloblaStyle />
+        <Table>
+          <Thead>
+            <tr className="tr">
+              <td className="td">번호</td>
+              <td className="td">이름</td>
+              <td className="td">생년월일</td>
+              <td className="td">성별</td>
+              <td className="td">직업</td>
+            </tr>
+          </Thead>
+          <tbody>
+            {this.state.customers ? this.state.customers.map(info => {
+                return (
+                  <Customer
+                    key={info.id}
+                    
+                    id={info.id}
+                    name={info.NAME}
+                    birthday={info.birthday}
+                    gender={info.gender}
+                    job={info.job}
+                  />
+                )
+              }) : ''}
+          </tbody>
+        </Table>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ management/server/server.js
@@ -0,0 +1,33 @@
+const fs =require('fs');
+const express = require('express');
+const app = express();
+const port = process.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
+
+app.get('/api/customers', (req, res) => {
+    connection.query(
+      'SELECT * FROM CUSTOMER',
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    )
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
